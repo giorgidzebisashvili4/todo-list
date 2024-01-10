f345a08f0e562a1508b7cb6a9569bc1926c0bc31
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-01-10 19:51:52 +0400

    simple to do list added

--- src/newToDo.js
+++ src/newToDo.js
@@ -7,3 +7,31 @@ function newToDo(title, description, dueDate, priority) {
 }
 
 export default newToDo;
+
+const ul = document.querySelector(".ulDiv>ul");
+const input = document.querySelector("#test");
+const button = document.querySelector(".testbtn");
+document.querySelector("#test").focus();
+
+button.addEventListener("click", () => {
+  const value1 = input.value;
+
+  if (value1 !== "") {
+    input.value = "";
+
+    const card = document.createElement("li");
+    const cardText = document.createElement("span");
+    const cardBtn = document.createElement("button");
+
+    cardText.textContent = value1;
+    cardBtn.textContent = "Delete";
+    cardBtn.addEventListener("click", () => {
+      ul.removeChild(card);
+    });
+
+    card.appendChild(cardText);
+    card.appendChild(cardBtn);
+    ul.appendChild(card);
+  }
+  input.focus();
+});
